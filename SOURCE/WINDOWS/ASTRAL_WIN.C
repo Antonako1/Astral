ff485f50b94db4fcd8b663b9bdc4f3ffc6d09e04
antonako <114558368+Antonako1@users.noreply.github.com>
2025-02-27 02:37:08 +0200

    Updated ASTRAL_CON implementation

--- SOURCE/WINDOWS/ASTRAL_WIN.C
+++ SOURCE/WINDOWS/ASTRAL_WIN.C
@@ -14,6 +14,7 @@ ASTRAL_CON.H
 ASTRAL_CONSOLE ASTRAL_CON_CREATE(ASTRAL_M_ARENA* ARENA) {
     ASTRAL_CONSOLE CONSOLE;
     CONSOLE.BUFFER = NULL;
+    CONSOLE.BUFFER_SIZE = 0;
     CONSOLE.ARENA = ARENA;
     CONSOLE.HANDLEOUT = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE.HANDLEIN = GetStdHandle(STD_INPUT_HANDLE);
@@ -21,34 +22,49 @@ ASTRAL_CONSOLE ASTRAL_CON_CREATE(ASTRAL_M_ARENA* ARENA) {
     CONSOLE.CON_MODE = 0;
     CONSOLE.WIDTH = GetSystemMetrics(SM_CXSCREEN);
     CONSOLE.HEIGHT = GetSystemMetrics(SM_CYSCREEN);
+    GetConsoleCP();
+    CONSOLE.OGOCHCP = GetConsoleOutputCP();
+    CONSOLE.OGCHCP = GetConsoleCP();
+    SetConsoleCP(CP_ACP);
+    SetConsoleOutputCP(CP_ACP);
+    GetConsoleMode(CONSOLE.HANDLEIN, (LPDWORD)&CONSOLE.OG_MODE);
+    CONSOLE.CON_MODE |= ENABLE_WINDOW_INPUT;
+    SetConsoleMode(CONSOLE.HANDLEIN, (DWORD)CONSOLE.CON_MODE);
     return CONSOLE;
 }
 
 AS_BOOLEAN ASTRAL_CON_APPLY_MODES(ASTRAL_CONSOLE* CONSOLE, AS_U64 MODES) {
-    DWORD mode = 0;
-    if(!GetConsoleMode(CONSOLE->HANDLEOUT, &mode)) {
-        return FALSE;
+    if (MODES & ASTRAL_CON_MODE_IGNORE_CTRL_C) {
+        SetConsoleCtrlHandler(NULL, TRUE);
+    } else {
+        SetConsoleCtrlHandler(NULL, FALSE);
     }
-    if(MODES & ASTRAL_CON_MODE_IGNORE_CTRL_C)    mode |= ENABLE_PROCESSED_INPUT;
-    if(MODES & ASTRAL_CON_MODE_LINE_WRAP)        mode |= ENABLE_WRAP_AT_EOL_OUTPUT;
-    if(MODES & ASTRAL_CON_MODE_CURSOR_VISIBLE)   mode |= ENABLE_PROCESSED_OUTPUT;
-    if(MODES & ASTRAL_CON_MODE_CURSOR_BLINK)     mode |= ENABLE_MOUSE_INPUT;
-    if(MODES & ASTRAL_CON_MODE_MOUSE_INPUT)      mode |= ENABLE_MOUSE_INPUT;
-    if(MODES & ASTRAL_CON_MODE_WINDOW_INPUT)     mode |= ENABLE_WINDOW_INPUT;
-    if(!SetConsoleMode(CONSOLE->HANDLEOUT, mode)) return FALSE;
-    CONSOLE->CON_MODE = MODES;
+    if (MODES & ASTRAL_CON_MODE_MOUSE_INPUT) {
+        DWORD sc_mode = ENABLE_EXTENDED_FLAGS;
+        if(!SetConsoleMode(CONSOLE->HANDLEIN, sc_mode)) return FALSE;
+        sc_mode = ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT;
+        if(!SetConsoleMode(CONSOLE->HANDLEIN, sc_mode)) return FALSE;
+    } else {
+        DWORD sc_mode = ENABLE_QUICK_EDIT_MODE | ENABLE_EXTENDED_FLAGS;
+        if(!SetConsoleMode(CONSOLE->HANDLEIN, sc_mode)) return FALSE;
+    }
+    return TRUE;
+}
 
+
+AS_BOOLEAN ASTRAL_CON_END_CONSOLE(ASTRAL_CONSOLE* CONSOLE) {
+    SetConsoleMode(CONSOLE->HANDLEIN, (DWORD)CONSOLE->OG_MODE);
+    SetConsoleMode(CONSOLE->HANDLEIN, ENABLE_QUICK_EDIT_MODE | ENABLE_EXTENDED_FLAGS);
+    SetConsoleCtrlHandler(NULL, FALSE);
+    CONSOLE->RUNNING = FALSE;
     return TRUE;
 }
 
 
 
-
-
-AS_U0 ASTRAL_CON_GET_EVENT(ASTRAL_CONSOLE* CONSOLE, ASTRAL_CONSOLE_EVENT* EVENT) {
+AS_U0 ASTRAL_CON_GET_EVENT(ASTRAL_CONSOLE* CONSOLE, ASTRAL_CON_EVENT* EVENT) {
     INPUT_RECORD input;
     if(!ReadConsoleInput(CONSOLE->HANDLEIN, &input, 1, (LPDWORD)&EVENT->EVENTS_READ)) {
-        WriteConsoleW(CONSOLE->HANDLEOUT, L"Error reading console input\n", 30, NULL, NULL);
         return;
     }
     switch(input.EventType) {
@@ -86,24 +102,26 @@ AS_U0 ASTRAL_CON_GET_EVENT(ASTRAL_CONSOLE* CONSOLE, ASTRAL_CONSOLE_EVENT* EVENT)
 
 AS_U16 ASTRAL_CON_CLEAR(ASTRAL_CONSOLE* CONSOLE) {
     DWORD mode = 0;
-    if(!GetConsoleMode(CONSOLE->HANDLEOUT, &mode)) {
+    if(!GetConsoleMode(CONSOLE->HANDLEIN, &mode)) {
         return (AS_U16)GetLastError();
     }
 
     const DWORD original_mode = mode;
     mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
 
-    if(!SetConsoleMode(CONSOLE->HANDLEOUT, mode)) {
+    if(!SetConsoleMode(CONSOLE->HANDLEIN, mode)) {
         return (AS_U16)GetLastError();
     }
 
+    SetConsoleCP(CONSOLE->OGCHCP);
+    SetConsoleOutputCP(CONSOLE->OGOCHCP);
     DWORD written = 0;
     PCWSTR clear_sequence = L"\x1b[2J";
     if(!WriteConsoleW(CONSOLE->HANDLEOUT, clear_sequence, 4, &written, NULL)) {
-        SetConsoleMode(CONSOLE->HANDLEOUT, original_mode);
+        SetConsoleMode(CONSOLE->HANDLEIN, original_mode);
         return (AS_U16)GetLastError();
     }
 
-    SetConsoleMode(CONSOLE->HANDLEOUT, original_mode);
+    SetConsoleMode(CONSOLE->HANDLEIN, original_mode);
     return 0;
 }
