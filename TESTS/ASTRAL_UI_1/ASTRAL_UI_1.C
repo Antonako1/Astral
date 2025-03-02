7a902cf0d65fc9cef92c9a00051aa03dcf041e6f
antonako <114558368+Antonako1@users.noreply.github.com>
2025-03-02 21:56:39 +0200

    New creation function and delete f

--- TESTS/ASTRAL_UI_1/ASTRAL_UI_1.C
+++ TESTS/ASTRAL_UI_1/ASTRAL_UI_1.C
@@ -2,15 +2,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+enum IDs {
+    AS_ROOT_ID=0,         // Root element reserves ID 0
+    AS_MAIN_BOX_ID,
+};
+
 int main(void) {
     
     ASTRAL_CONSOLE *console = ASTRAL_CON_CREATE();
     ASTRAL_CON_APPLY_MODES(console, ASTRAL_CON_MODE_MOUSE_INPUT | ASTRAL_CON_MODE_IGNORE_CTRL_C);
 
-    printf("Console root id: %d\n", console->ROOT->ID);
+    printf("Console root id: %llu\n", console->ROOT->ID);
     printf("Console root height: %llu\n", console->ROOT->STYLING.SIZE.HEIGHT);
     printf("Console root width: %llu\n", console->ROOT->STYLING.SIZE.WIDTH);
-    
+
+    ASTRAL_CON_UI_ELEM_SET_TEXT(console->ROOT, ASTRAL_STR_CREATE_EX(STR_MAX_COMBO("Hello, world!")));
+
+    ASTRAL_CON(CONSOLE,
+        ASTRAL_CON_CE(
+            ASTRAL_CON_UI_ELEM_TYPE_BOX,
+            ASTRAL_DEF_SUB_TYPE,
+            AS_MAIN_BOX_ID,
+            ASTRAL_CON_CREATE_COORD(0,0),
+            ASTRAL_CON_CREATE_STYLING(
+                ASTRAL_CON_CREATE_COLOUR_F_B_M(RGBA(0,0,0,0),RGBA(0,0,0,0)),
+                ASTRAL_CON_CREATE_MARGIN(0,0,0,0),
+                ASTRAL_CON_CREATE_PADDING(0,0,0,0),
+                0, 0, ASTRAL_CON_CREATE_COLOUR_F_B_M(RGBA(0,0,0,0),RGBA(0,0,0,0)), BORDER_STYLE_DEFAULT,
+                ASTRAL_CON_CREATE_SIZE(10, 10)
+            ),
+            NULLPTR,
+
+        )
+    )
+
     ASTRAL_CON_EVENT input;
     AS_U64 i = 0;
     while(ASTRAL_CON_IS_RUNNING(console)) {
